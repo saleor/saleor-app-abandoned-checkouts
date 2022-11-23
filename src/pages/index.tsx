3fddb86fe003f7552d413d12a37fd1998c02f84c
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2022-11-23 09:13:56 +0100

    feat: :tada: create abandoned-checkouts app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,97 +1,58 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
 import { NextPage } from "next";
-import { useAppBridge } from "@saleor/app-sdk/app-bridge";
-import { Button } from "@saleor/macaw-ui";
-import dynamic from "next/dynamic";
-import { MouseEventHandler, useEffect, useState } from "react";
+import { useFetchAllCheckoutsQuery } from "../../generated/graphql";
 
-const ClientContent = dynamic(() => import("../DashboardActions"), {
-  ssr: false,
-});
+const AbandonedCheckoutsPage: NextPage = () => {
+  const [{ data, error }] = useFetchAllCheckoutsQuery();
 
-/**
- * This is page publicly accessible from your app.
- * You should probably remove it.
- */
-const IndexPage: NextPage = () => {
-  const { appBridgeState, appBridge } = useAppBridge();
-  const [mounted, setMounted] = useState(false);
+  if (!data?.checkouts) {
+    return <div>Loading...</div>;
+  }
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const handleLinkClick: MouseEventHandler<HTMLAnchorElement> = (e) => {
-    /**
-     * In iframe, link can't be opened in new tab, so Dashboard must be a proxy
-     */
-    if (appBridgeState?.ready) {
-      e.preventDefault();
-
-      appBridge?.dispatch({
-        type: "redirect",
-        payload: {
-          newContext: true,
-          actionId: "redirect-to-external-resource",
-          to: e.currentTarget.href,
-        },
-      });
-    }
-
-    /**
-     * Otherwise, assume app is accessed outside of Dashboard, so href attribute on <a> will work
-     */
-  };
+  if (error) {
+    return <div>{error.message}</div>;
+  }
 
   return (
     <div>
-      <h1>Welcome to Saleor App Template (Next.js) 🚀</h1>
-      <p>This is a boilerplate you can start with, to create an app connected to Saleor</p>
-      <h2>Resources</h2>
-      <ul>
-        <li>
-          <a
-            onClick={handleLinkClick}
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/saleor/app-examples"
-          >
-            App Examples repository
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="https://github.com/saleor/saleor-app-sdk">
-            Saleor App SDK
-          </a>
-        </li>
-        <li>
-          <a
-            onClick={handleLinkClick}
-            target="_blank"
-            href="https://docs.saleor.io/docs/3.x/developer/extending/apps/key-concepts"
-            rel="noreferrer"
-          >
-            Apps documentation
-          </a>
-        </li>
-        <li>
-          <a
-            onClick={handleLinkClick}
-            target="_blank"
-            href="https://github.com/saleor/saleor-cli"
-            rel="noreferrer"
-          >
-            Saleor CLI
-          </a>
-        </li>
-      </ul>
+      <h1>Abandoned Checkouts</h1>
 
-      {appBridgeState?.ready && mounted ? (
-        <ClientContent />
-      ) : (
-        <p>Install this app in your Dashboard and check extra powers!</p>
-      )}
+      <main>
+        {data?.checkouts.edges.length > 0 ? (
+          <TableContainer component={Paper}>
+            <Table aria-label="checkouts table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>No.</TableCell>
+                  <TableCell>Checkout Id</TableCell>
+                  <TableCell>Created At</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.checkouts.edges.map((row, i) => (
+                  <TableRow key={row.node.id}>
+                    <TableCell>{i + 1}.</TableCell>
+                    <TableCell>{row.node.id}</TableCell>
+                    <TableCell>{row.node.created}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <div>No data to display...</div>
+        )}
+      </main>
     </div>
   );
 };
 
-export default IndexPage;
+export default AbandonedCheckoutsPage;
